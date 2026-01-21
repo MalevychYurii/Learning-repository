36a5a970d7eae919cf6c063a190148c4acbc2c11
Yurii <yuramvm1@gmail.com>
2026-01-21 20:36:20 +0200

    closure practice

--- JS/test.js
+++ JS/test.js
@@ -1,60 +1,17 @@
-'use strict';
-
-function greet(name) {
-    console.log(`Hello, ${name}!`)
-}
-
-greet('Yurii');
-
-function calc(a, b, operation) {
-    let result;
+function counter() {
+    let num = 0;
     
-    if (operation === '-') {
-        result = a - b;
-    } else if (operation === '+') {
-        result = a + b;
-    } else if (operation === '*') {
-        result = a * b;
-    } else if (operation === '/') {
-        result = a / b;
+    function counterPlus() {
+        num++;
+        console.log(num);
     }
-
-    return result
+    
+    counterPlus(); // Викликаємо функцію
+    return num;    // Повертаємо оновлене значення
 }
 
-console.log(calc(4, 5, '-'))
-
-function isEven(num) {
-
-    if (num % 2 === 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-console.log(isEven(5));
-
-function sumArray(arr) {
-    let sum = 0;
-
-    for (let i = 0; i < arr.length; i++) {
-        sum = sum + arr[i];
-    }
-
-    return sum;
-}
-
-console.log(sumArray([1, 1, 1, 1]));
-
-function findMax(arr) {
-    return Math.max(...arr);
-}
-
-console.log(findMax([1, 2, 3]));
-
-function getEvenNumbers(arr) {
-    return arr.filter(num => num % 2 === 0);
-}
-
-console.log(getEvenNumbers([1,2,3]))
+const finalNum = counter(); // Виведе 1
+console.log(finalNum); // 1
+console.log(finalNum);
+console.log(finalNum);
+console.log(finalNum);
