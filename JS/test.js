23de6ef7ae8a1a4c50d59a1e39e60a7b98c9a276
Yurii <yuramvm1@gmail.com>
2026-01-26 21:41:01 +0200

    tasks practice

--- JS/test.js
+++ JS/test.js
@@ -1,16 +1,45 @@
-'use strict';
+const array = [1, 2, 2, 3, 1, 5, 12];
 
-function test() {
-    let arr = [1,2,3,4,5,6,7,8,9,10];
-    let arr2 = [];
+function removeDuplicate() {
+    
+    let seen = [];
+    let duplicates = [];
 
-    arr.forEach((num) => {
-        if (num % 2 === 0) {
-            arr2.push(num);
+    for (let value of array) {
+        if (seen.includes(value)) {
+            duplicates.push(value);
+        } else {
+            seen.push(value);
         }
-    });
+    }
 
-    return arr2;
+    console.log(`seen: ${seen}, duplicates: ${duplicates}`)
 }
 
-console.log(test());
+removeDuplicate();
+
+let arr = [];
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('FizzBuzz');
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
+    } else {
+        console.log(i);
+    }
+}
+
+function validatePassword(password) {
+    
+    if (password.length < 8) return false;
+
+    if (!/[0-9]/.test(password)) return false;
+    if (!/[A-Z]/.test(password)) return false;
+
+    return true;
+}
+
+console.log(validatePassword("password")); 
